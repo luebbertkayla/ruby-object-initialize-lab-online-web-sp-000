36e1c204b7e45d1f903269b8cbec3ce68fbc7415
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-14 05:39:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,27 +1,4 @@
 class Dog 
   def initialize(dog_name)
     @dog_name = dog_name
-  end
-  
-  def dog_name=(dog_name)
-    @dog_name = dog_name
-  end
-  
-  def dog_name
-    @dog_name
-  end
-  
-  def initialize(breed)
     @breed = breed
-  end
-  
-  def breed=(breed)
-    @breed = breed
-  end
-  
-  def breed
-    @breed
-  end
-end
-
-Fido = Dog.new("Fido", "Mutt")
